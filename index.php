81d3f019a10c905d528f15d04e7e529da56422ed
Arthur <arturskomarohov@gmail.com>
2024-02-14 20:34:18 +0200

    feat: set project structure

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
-<?php
 
-echo "new file";
+Введите свою ссылку ниже
+и мы создадим для вас короткую версию,
+которую легко передать другу:
+
+
+<form action="link.php">
+    <input name="link">
+    <input type="submit" value="send">
+</form>
--- link.php
+++ link.php
@@ -1,25 +1,36 @@
-function find_short_link($short_link = '') {
-    // проверка полученной ссылки на существование в содержимом файлов
+<?php
+function find_by_short_link($short_link = '')
+{
+    //     проверка полученной ссылки на существование в содержимом файлов
+//    return [
+//        'short_link'=> $short_link,
+//        'long_link'=> "slkjf" ];
 }
 
-function find_full_link($full_link = '') {
+
+
+
+
+function find_by_full_link($full_link = '') {
     // проверка полученной ссылки на существование в именах файлов
 }
 
-function create_short_link():string {
+function create_short_link() {
     // создание короткой ссылки
 }
 
-function save_link($short_link = '', $full_link = ''): {
-    // сохранение ссылки в файл
-}function save_link($short_link = '', $full_link = ''): {
+function save_link($short_link = '', $full_link = '') {
     // сохранение ссылки в файл
 }
+//function save_link($short_link = '', $full_link = ''): {
+//    // сохранение ссылки в файл
+//}
 
 // BEGIN
-$input_data = $_GET['link'];
+$input_full_link = $_GET['link'];
 
 // TODO
 
 echo "Your link is: <br>
-/go.php/ksjdhfkjas";
+/go.php/ksjdhfkjas <br>" ;
+echo "Your long link is {$input_full_link}";
