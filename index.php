74dd8c185c3a7cb49fc0a15168a6a75f3ffa7ac3
ArchiScript <arturskomarohov@gmail.com>
2024-02-15 14:37:42 +0200

    feat(link): develop functions to manipulate folders and files, chekc files

--- index.php
+++ index.php
@@ -5,7 +5,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style/style.css">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/favicons/favicon-16x16.png">
+    <link rel="stylesheet" href="assets/style/style.css">
     <title>short-url-hub</title>
 </head>
 
@@ -18,7 +20,7 @@
                 которую легко передать другу:</div>
 
             <form class="app__form" action="link.php">
-                <input type="url" name="link" placeholder="https//example.com">
+                <input type="url" name="link" placeholder="https//example.com" required>
                 <input type="submit" value="send">
             </form>
 
--- link.php
+++ link.php
@@ -1,36 +1,105 @@
 <?php
-function find_by_short_link($short_link = '')
+function find_by_short_link($short_link = ''): string | bool
+{
+    // проверка полученной ссылки на существование в именах файлов
+    $folderPath = "data";
+    $files      = scandir($folderPath);
+    if ($short_link) {
+        foreach ($files as $file) {
+            if ($file !== '.' && $file !== '..') {
+                if ($file == $short_link) {
+                    return $folderPath . '/' . $file;
+                } else {
+                    return false;
+                }
+            }
+        }
+    }
+}
+
+function find_by_full_link($full_link = ''): string | bool
 {
     //     проверка полученной ссылки на существование в содержимом файлов
-//    return [
-//        'short_link'=> $short_link,
-//        'long_link'=> "slkjf" ];
+    $folderPath = "data";
+    $files      = scandir($folderPath);
+
+    if ($full_link) {
+        $search_content = function ($i = 0) use ($files, $full_link, $folderPath, &$search_content) {
+            for ($i; $i < count($files); $i++) {
+                $file = $files[$i];
+
+                if ($file !== '.' && $file !== '..') {
+                    $filePath    = $folderPath . '/' . $file;
+                    $fileContent = file_get_contents($filePath);
+
+                    if ($fileContent === $full_link) {
+                        echo "Эта ссылка уже находится в базе </br>";
+                        print_r($filePath . ": " . $fileContent);
+                        return $filePath;
+                    } else {
+                        $result = $search_content($i + 1);
+
+                        if ($result !== false) {
+                            return $result;
+                        }
+                    }
+                }
+            }
+            return false;
+        };
+        return $search_content();
+    }
+    return false;
 }
 
-
-
-
-
-function find_by_full_link($full_link = '') {
-    // проверка полученной ссылки на существование в именах файлов
-}
-
-function create_short_link() {
+function create_short_link(int $length): string
+{
     // создание короткой ссылки
+    $random_bytes = random_bytes(5);
+    $hex_link     = bin2hex($random_bytes);
+    $hash         = hash('sha256', $hex_link);
+
+    $shortLink = substr($hash, 0, $length);
+    return $shortLink;
 }
 
-function save_link($short_link = '', $full_link = '') {
+function save_link($short_link = '', $full_link = '')
+{
     // сохранение ссылки в файл
-}
-//function save_link($short_link = '', $full_link = ''): {
-//    // сохранение ссылки в файл
-//}
 
-// BEGIN
-$input_full_link = $_GET['link'];
+    if ($short_link && $full_link) {
+        if (!find_by_short_link($short_link) && !find_by_full_link($full_link)) {
+            $filePath = "data/{$short_link}.txt";
+            file_put_contents($filePath, $full_link, LOCK_EX);
+        }
+
+    }
+
+}
+
+function get_full_link(): string
+{
+    if (isset($_GET['link'])) {
+        $input_full_link = $_GET['link'];
+
+    }
+    return $input_full_link;
+
+}
+
+function render_full(string $link): string
+{
+    return "<a href=\"{$link}\" target=\"_blank\">{$link}</a><br />";
+}
+
+function render_short(string $link, string $visibleLink): string
+{
+    return "<a href=\"{$link}\" target=\"_blank\">https://{$visibleLink}</a><br />";
+}
 
 // TODO
 
-echo "Your link is: <br>
-/go.php/ksjdhfkjas <br>" ;
-echo "Your long link is {$input_full_link}";
+echo "Your long link is: " . render_full(get_full_link());
+echo "Your short link is: " . render_short(get_full_link(), create_short_link(10)) . "<br />";
+
+save_link(create_short_link(10), get_full_link());
