c2ccd5b801290ae3c7ba31c72e7891db8adc6cbf
ArchiScript <arturskomarohov@gmail.com>
2024-02-15 09:41:24 +0200

    style: add index page styles

--- index.php
+++ index.php
@@ -1,10 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
 
-Введите свою ссылку ниже
-и мы создадим для вас короткую версию,
-которую легко передать другу:
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
+    <title>short-url-hub</title>
+</head>
+
+<body>
+    <div class="bg-name">short-url-hub</div>
+    <main class="app">
+        <div class="app__inner">
+            <div class="app__title">Введите свою ссылку ниже -
+                и мы создадим для вас короткую версию,
+                которую легко передать другу:</div>
+
+            <form class="app__form" action="link.php">
+                <input type="url" name="link" placeholder="https//example.com">
+                <input type="submit" value="send">
+            </form>
 
 
-<form action="link.php">
-    <input name="link">
-    <input type="submit" value="send">
-</form>
+        </div>
+    </main>
+</body>
+
+</html>
